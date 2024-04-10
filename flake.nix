0cb325f8319b12f9582d409cc3198c74c8c44f20
Aidan Gauland <aidalgol@fastmail.net>
2024-04-10 17:30:04 +1200

    Use Steam fetcher from branch
    Addresses #7

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     steam-fetcher = {
-      url = "github:nix-community/steam-fetcher";
+      url = "github:nix-community/steam-fetcher?ref=fetch-hash-issue";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
